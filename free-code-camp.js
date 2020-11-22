16364e1dbef5a624200b52dccc8664ff2abf8b79
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2020-11-21 22:41:13 -0600

    continuing freecodecamp exercises..

--- free-code-camp.js
+++ free-code-camp.js
@@ -12,3 +12,21 @@ myArray.shift();
 // Only change code below this line
 //*****************************************//
 //35.//
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+//*****************************************//
+//36.//
+var myList = [["Chocolate Bar", 15], ["string2", 1], ["string3", 2],
+    ["string4", 3], ["string5", 4]];
+//*****************************************//
+//37.//
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
+//*****************************************//
+//38.//
