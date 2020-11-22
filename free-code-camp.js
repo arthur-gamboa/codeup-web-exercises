a129ba362a5652aadc3f03e1241f2e77965f51d1
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2020-11-21 22:19:52 -0600

    continuation on freecodecamp.org

---
+++ free-code-camp.js
@@ -0,0 +1,14 @@
+//33.//
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+var removedFromMyArray;
+//*****************************************//
+//34.//
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+// Only change code below this line
+//*****************************************//
+//35.//
